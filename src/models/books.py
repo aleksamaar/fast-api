105edf9a67989da07e7352ac8600da7c0773e538
aleksamaar <themaaars@gmail.com>
2026-03-13 00:15:24 +0100

    Add FastAPI Book-Seller application

---
+++ src/models/books.py
@@ -0,0 +1,24 @@
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import BaseModel
+
+COUNTER = 0
+fake_storage = {}
+
+
+class Book(BaseModel):
+    __tablename__ = "books_table"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    author: Mapped[str] = mapped_column(String(50), nullable=False)
+    year: Mapped[int] = mapped_column(nullable=True)
+    pages: Mapped[int] = mapped_column(nullable=False)
+
+    seller_id: Mapped[int] = mapped_column(
+        ForeignKey("sellers_table.id"),
+        nullable=False
+    )
+
+    seller: Mapped["Seller"] = relationship("Seller", back_populates="books")
